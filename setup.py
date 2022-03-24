010d1935616b434e1bc31aa8ab3cc489b07703ea
Weipeng Zhang <wp.zhang@hotmail.com>
2022-03-23 20:19:49 -0400

    :see_no_evil: add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='HandyRec',
+    version='0.1.0',
+    author='Weipeng Zhang',
+    author_email='wp.zhang@hotmail.com',
+    package=['handyrec'],
+)
