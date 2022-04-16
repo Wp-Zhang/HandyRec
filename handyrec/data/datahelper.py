2ac333d0f295236bd85d72adef2d8bfd047b99b5
Weipeng Zhang <wp.zhang@hotmail.com>
2022-04-16 00:07:36 -0400

    :fire: remove deprecated code

--- handyrec/data/datahelper.py
+++ handyrec/data/datahelper.py
@@ -1,10 +1,9 @@
-from typing import Dict, List
+from typing import Dict
 from abc import ABC, abstractmethod
-import numpy as np
 
 
 class DataHelper(ABC):
-    """Abstract class for data loading, preprocesing, and dataset generating
+    """Abstract class for data loading and preprocesing
 
     Attributes
     ----------
@@ -44,56 +43,3 @@ class DataHelper(ABC):
         Dict
             A data dict with three keys: ``user``, ``item``, and ``inter``.
         """
-
-    @classmethod
-    def get_feature_dim(
-        cls,
-        data: Dict,
-        user_features: List[str],
-        item_features: List[str],
-        interact_features: List[str],
-    ) -> Dict[str, int]:
-        """Generate a dictionary containing feature dimension info.
-
-        Parameters
-        ----------
-        data : Dict
-            Dataset dictionary.
-        user_features : List[str]
-            User feature list.
-        item_features : List[str]
-            Item feature list.
-        interact_features : List[str]
-            User-item interaction feature list.
-
-        Returns
-        -------
-        Dict[str, int]
-            A dictionary containing feature dimension info, ``{feature: dim}``.
-
-        Raises
-        ------
-        KeyError
-            If ``user``,``item``, or ``inter`` is not in ``data.keys()``
-        """
-        if len(set(["user", "item", "inter"]) & set(data.keys())) != 3:
-            raise KeyError("`user`,`item`, and `inter` should be keys of data")
-
-        feature_dim = {}
-        for feat in user_features:
-            try:
-                feature_dim[feat] = np.max(data["user"][feat]) + 1
-            except:
-                pass
-        for feat in item_features:
-            try:
-                feature_dim[feat] = np.max(data["item"][feat]) + 1
-            except:
-                pass
-        for feat in interact_features:
-            try:
-                feature_dim[feat] = np.max(data["inter"][feat]) + 1
-            except:
-                pass
-
-        return feature_dim
