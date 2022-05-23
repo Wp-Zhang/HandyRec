4e35f697351cbe54ba28a29af92bd3d2a5804234
Weipeng Zhang <wp.zhang@hotmail.com>
2022-05-23 12:26:51 -0400

    :rocket: update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from handyrec import __version__
+
+REQUIRES = ["numpy>=1.21.5", "pandas>=1.3.4", "python_box>=6.0.2", "tensorflow>=2.6.0"]
 
 setup(
-    name='HandyRec',
-    version='0.1.0',
-    author='Weipeng Zhang',
-    author_email='wp.zhang@hotmail.com',
-    package=['handyrec'],
+    name="handyrec",
+    version=__version__,
+    author="Weipeng Zhang",
+    author_email="wp.zhang@hotmail.com",
+    packages=find_packages(
+        exclude=[
+            "tests",
+            "tests.models",
+            "tests.layers",
+            "tests.data",
+            "tests.features",
+            "tests.models.ranking",
+            "tests.models.retrieval",
+        ]
+    ),
+    install_requires=REQUIRES,
 )
